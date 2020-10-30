ef0e7e6bc638a7d11263585150ecc5caf135e71b
raceybe <raceybe@gmail.com>
2020-10-30 11:16:44 -0400

    Update and rename config.example.php to testConfig.example.php

--- testConfig.example.php
+++ testConfig.example.php
@@ -2,11 +2,11 @@
 /*********************************************************************************
 *Config Example
 *
-*Make sure this file is in the same folder as testclient.php and is called config.php
+*Make sure this file is in the same folder as testclient.php and is called testConfig.php
 *********************************************************************************/
 //ddns client configuration details
 $myip='1.2.3.4';
 $hostname='user1host.example.net';
 $login='user1';
 $password='user1password';
-$server='ddns.example.com';
+$serverHostname='ddns.example.com';
