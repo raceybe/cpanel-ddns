d440ea3d6b63689c1d95198c5c9737faece331c7
raceybe <raceybe@gmail.com>
2020-10-30 11:34:02 -0400

    Update and rename nic/config.example.php to serverConfig.example.php
    added cpToken variable, and stripped https:// from cpServer so it is just the bare hostname since https:// is applied to the request elsewhere

--- serverConfig.example.php
+++ serverConfig.example.php
@@ -1,13 +1,17 @@
 <?php
 /*********************************************************************************
-*Config Example
+*Server Config Example
 *
-*Make sure this file is in the same folder as update.php and is called config.php
+*Make sure this file is in the same folder as update.php and is called serverConfig.php
+*The update script works with EITHER the cPanel password OR a cPanel API token
+*If both password and API token are provided the API token is used
+*The username is required for both methods of authentication
 *********************************************************************************/
 //cPanel API user, password, server, and port
 $cpUser = 'cpanelusername';
 $cpPassword = 'cpanelpassword';
-$cpServer = 'https://server.example.com';
+$cpToken='cpanelapitoken';
+$cpServer = 'server.example.com';
 $cpPort = '2083';
 
 //Authorized ddns users, passwords, and hostnames
