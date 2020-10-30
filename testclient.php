7ca6142f5f4fa5e794703c3852c702ddac9a927c
raceybe <raceybe@gmail.com>
2020-10-30 11:18:35 -0400

    Update testclient.php

--- testclient.php
+++ testclient.php
@@ -1,19 +1,16 @@
 <?php
-require "config.php";
-
-$base64auth=base64_encode($login.":".$password);
-$header[0] = "Authorization: Basic $base64auth";
+require 'testConfig.php';
 
 //Setup curl object
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 
-curl_setopt($curl, CURLOPT_URL, "https://$server/nic/update?hostname=$hostname&myip=$myip");
+curl_setopt($curl, CURLOPT_URL, "https://$serverHostname/nic/update?hostname=$hostname&myip=$myip");
 $response = curl_exec($curl);
 
 curl_close($curl);
 
-echo "<pre>";
+echo '<pre>';
 echo $response;
-echo "</pre>";
+echo '</pre>';
